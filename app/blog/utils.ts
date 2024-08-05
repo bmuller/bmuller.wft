9672a9c9ee74b8cdb487425c71fa897ddb4c4f7f
Brian Muller <bamuller@gmail.com>
2024-08-05 19:42:20 -0400

    Fix type errors

--- app/blog/utils.ts
+++ app/blog/utils.ts
@@ -7,6 +7,7 @@ type Metadata = {
   summary: string
   image?: string
   imageCredit?: string
+  imageCreditLink?: string  
 }
 
 function parseFrontmatter(fileContent: string) {
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 import { Montserrat, Roboto } from 'next/font/google'
-import { ReactElement } from 'react'
 
 const roboto = Roboto({
   weight: '400',
@@ -53,7 +52,7 @@ export const metadata: Metadata = {
   }
 }
 
-export default ({ children }: ReactElement) => (
+export default ({ children }: Readonly<{ children: React.ReactNode }>) => (
   <html lang="en" className={`text-black bg-white dark:text-white dark:bg-black ${roboto.variable} ${montserrat.variable}`}>
     <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
       <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
